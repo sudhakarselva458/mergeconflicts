ccaafdbeb14821a292c2883a0bce665f26da3605
sudhakarselva458 <sudhakarselva458@gmail.com>
2024-01-31 17:09:42 +0000

    upload

--- details.py
+++ details.py
@@ -1,1 +1,23 @@
+from tabulate import tabulate
+import mysql.connector
+mydb=mysql.connector.connect(
+    host="localhost",
+    username="root",
+    password="Sudhakar@95",
+    database="learning"
+)
+def insert(name,city,grade):
+    pass
+def update(name,city,grade):
+    pass
+def select():
+    pass
+def delete(id):
+    pass
+
+while true:
+    print("1.insert")
+    print("2.upadte")
+    print("3.select")
+    print("4.delete")
 
